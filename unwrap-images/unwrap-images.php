90226321b4d87e3f4dd419584900e91213d12526
paulrudy <1110792+paulrudy@users.noreply.github.com>
2020-01-02 15:05:00 -0800

    fix to respect global process_content value

--- unwrap-images/unwrap-images.php
+++ unwrap-images/unwrap-images.php
@@ -18,40 +18,39 @@ class UnwrapImagesPlugin extends Plugin
     {
         $page = $event['page'];
         $pageobject = $this->grav['page'];
-        $processcontent = $this->config->get
-        ('plugins.unwrap-images.process_content');
         $class = $this->config->get
         ('plugins.unwrap-images.class');
-
+        $processcontent = $this->config->get
+        ('plugins.unwrap-images.process_content');
         if (isset ($pageobject->header()->unwrap_images['process_content']))  {
             $processcontent = $pageobject->header()->unwrap_images['process_content'];
-            if ($processcontent == true) {
-                $buffer = $page->content();
-                $url = $page->url();
-                $doc = new DOMDocument();
+        }
+        if ($processcontent == true) {
+            $buffer = $page->content();
+            $url = $page->url();
+            $doc = new DOMDocument();
 
-                // Hack to force DOMDocument to load the HTML using UTF-8:
-                $doc->loadHTML('<?xml encoding="UTF-8">'.$buffer); 
+            // Hack to force DOMDocument to load the HTML using UTF-8:
+            $doc->loadHTML('<?xml encoding="UTF-8">'.$buffer); 
 
-                $xpath = new DOMXpath($doc);
-                $elements = $xpath->query("//img[contains(@class,'$class')]");
+            $xpath = new DOMXpath($doc);
+            $elements = $xpath->query("//img[contains(@class,'$class')]");
 
-                foreach  ($elements as $element){
-                    $parentnode = $element->parentNode;
+            foreach  ($elements as $element){
+                $parentnode = $element->parentNode;
 
-                    // if parent is <a>, shift up one DOM level to find <p> wrapper if present:
-                    if ($element->parentNode->nodeName == 'a') {
-                        $parentnode = $parentnode->parentNode;
-                        $element = $element->parentNode;
-                    }
-                    $parentnodetype = $parentnode->nodeName;
-                    if ($parentnodetype == 'p') {
-                        $parentnode->parentNode->replaceChild($element,$parentnode);                  
-                    }
+                // if parent is <a>, shift up one DOM level to find <p> wrapper if present:
+                if ($element->parentNode->nodeName == 'a') {
+                    $parentnode = $parentnode->parentNode;
+                    $element = $element->parentNode;
                 }
-                $buffer = $doc->saveHTML();
-                $page->setRawContent($buffer);
+                $parentnodetype = $parentnode->nodeName;
+                if ($parentnodetype == 'p') {
+                    $parentnode->parentNode->replaceChild($element,$parentnode);                  
+                }
             }
+            $buffer = $doc->saveHTML();
+            $page->setRawContent($buffer);
         }
     }
 }
