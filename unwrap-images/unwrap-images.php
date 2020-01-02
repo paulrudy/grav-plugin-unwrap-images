ed82f21592cf8fc7185bc914e139405697b7d55b
paulrudy <1110792+paulrudy@users.noreply.github.com>
2020-01-02 14:37:39 -0800

    change search method from regex to DOM

--- unwrap-images/unwrap-images.php
+++ unwrap-images/unwrap-images.php
@@ -1,6 +1,8 @@
 <?php
 namespace Grav\Plugin;
 
+use DOMDocument;
+use DOMXpath;
 use Grav\Common\Plugin;
 use RocketTheme\Toolbox\Event\Event;
 
@@ -16,15 +18,36 @@ class UnwrapImagesPlugin extends Plugin
     {
         $page = $event['page'];
         $pageobject = $this->grav['page'];
-        $processcontent = $this->config->get('plugins.unwrap-images.process_content');
+        $processcontent = $this->config->get
+        ('plugins.unwrap-images.process_content');
+
         if (isset ($pageobject->header()->unwrap_images['process_content']))  {
             $processcontent = $pageobject->header()->unwrap_images['process_content'];
             if ($processcontent == true) {
                 $buffer = $page->content();
                 $url = $page->url();
-                $buffer = preg_replace("/<p>\s*?(<a .*<img.*<\/a>|<img.*)?\s*<\/p>/",
-                    "$1",
-                    $buffer);
+                $doc = new DOMDocument();
+
+                // Hack to force DOMDocument to load the HTML using UTF-8:
+                $doc->loadHTML('<?xml encoding="UTF-8">'.$buffer); 
+
+                $xpath = new DOMXpath($doc);
+                $elements = $xpath->query("//img");
+
+                foreach  ($elements as $element){
+                    $parentnode = $element->parentNode;
+
+                    // if parent is <a>, shift up one DOM level to find <p> wrapper if present:
+                    if ($element->parentNode->nodeName == 'a') {
+                        $parentnode = $parentnode->parentNode;
+                        $element = $element->parentNode;
+                    }
+                    $parentnodetype = $parentnode->nodeName;
+                    if ($parentnodetype == 'p') {
+                        $parentnode->parentNode->replaceChild($element,$parentnode);                  
+                    }
+                }
+                $buffer = $doc->saveHTML();
                 $page->setRawContent($buffer);
             }
         }
